3f23c3cff75a757e8ac4a043a7e2858885af60ca
sun30812 <sun30812@naver.com>
2023-05-10 16:37:19 +0900

    오류 메세지 한글화

--- functions/main.py
+++ functions/main.py
@@ -69,7 +69,7 @@ def update_attendance(event: firestore_fn.Event[firestore_fn.Change[firestore_fn
 class SubjectNotFoundError(Exception):
 
     def __init__(self):
-        super().__init__('Can not found subject or time limit exceed')
+        super().__init__('강의를 찾을 수 없습니다.(출결 시간 초과)')
         print('Can not get subject info (subject no exist.)')
 
 
@@ -77,7 +77,7 @@ class ProfessorNotFoundError(Exception):
     """강의자 정보를 찾을 수 없을 때 발생하는 예외"""
 
     def __init__(self):
-        super().__init__('Can not found professor info')
+        super().__init__('강의자 정보를 확인할 수 없습니다.')
         print('Can not find professor with id')
 
 
@@ -85,7 +85,7 @@ class DuplicateAttendanceError(Exception):
     """출결 정보가 중복될 때 발생하는 예외"""
 
     def __init__(self) -> None:
-        super().__init__('You have already completed attendance check.')
+        super().__init__('이미 출결 처리된 과목입니다.')
         print('already completed attendance check')
 
 
