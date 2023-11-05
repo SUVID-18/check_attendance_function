43997b9cf7716faecef3db8bdd4d221dd0d94927
sun30812 <sun30812@naver.com>
2023-11-05 21:14:30 +0900

    불필요한 함수 제거
    해당 함수 대신 Firebase API를 통해 작업이 가능하여 제거함

--- functions/main.py
+++ functions/main.py
@@ -7,38 +7,8 @@ from google.cloud.firestore_v1 import Client, base_query
 from attendance import Attendance
 from attendance_error import DuplicateAttendanceError
 from student import Student
-from subject import Subject
 
 initialize_app()
-
-
-@https_fn.on_call()
-def check_available_subjects(request: https_fn.CallableRequest):
-    """
-    강의실에서 강의가 진행되는 모든 과목의 정보를 가져오는 함수
-    Args:
-        request: Firebase Function 요청에 대한 정보가 들어간다.(자동으로 할당되는 매개변수)
-    Notes:
-        해당 함수는 직접 호출이 아닌 Firebase Function에 의해 호출되며 호출 시 아래와 같은 매개변수가 필요하다
-
-        * tag_uuid: 태그(강의실) UUID
-        * day_week: 정수로 표기된 특정 요일(0: 월요일, 1: 화요일, ... , 6: 일요일)
-
-    Returns:
-        특정 강의실에서 진행되는 강의의 목록이 반환된다. 태그 UUID에 해당되는 과목이 없는 경우 오류코드와 함께 오류에 관한 정보를 반환한다.
-    """
-    client: Client = firestore.client()
-    args = request.data
-    snapshot = client.collection('subjects') \
-        .where(filter=base_query.FieldFilter('tag_uuid', '==', args.get('tag_uuid'))) \
-        .where(filter=base_query.FieldFilter('day_week', '==', args.get('day_week'))) \
-        .get()
-    try:
-        subject_iterator = iter(snapshot)
-        subjects = [Subject(subject).to_json() for subject in subject_iterator]
-        return subjects
-    except():
-        raise https_fn.HttpsError(code=https_fn.FunctionsErrorCode.NOT_FOUND, message='강의실이 존재하지 않습니다.')
 
 
 @https_fn.on_call()
